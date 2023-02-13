9b5a4e1039181435b2a98bbf645bf9fe45370d74
Uladzislau <vladick.tsemenko@gmail.com>
2023-02-13 09:36:58 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -37,8 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const BINARY_TABLE = {
+    '00': '',
+    '10': '.',
+    '11': '-'
+}
+
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let strArray = [];
+
+    for(let i = 0; i < expr.length; i += 10) {
+        let temp = expr.slice(i, i + 10);
+        let tempStr = '';
+
+        if (temp == '**********') {strArray.push(' '); continue;}
+        
+        for(let j = 0; j < temp.length; j += 2) {
+            let item = temp[j] + temp[j + 1];
+            tempStr += BINARY_TABLE[item]
+        }
+
+        strArray.push(tempStr)
+    }
+
+    for(let elem of strArray) {
+        if(elem != ' ') result += MORSE_TABLE[elem];
+        else result += ' ';
+    }
+    
+    return result;
 }
 
 module.exports = {
